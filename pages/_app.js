4b47f704fbf0eae1df7bca0ba7587a7d30cffeef
Phnxss <fpsslab@gmail.com>
2025-01-21 12:48:51 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,18 @@ MyApp.getInitialProps = async (appContext) => {
   const cookies = new Cookies(appContext.ctx.req.headers.cookie);
   const password = cookies.get(consts.SiteReadCookie) ?? '';
 
-  if (password === 'letmein') {
+  // Define 30 different passwords
+  const validPasswords = [
+    'password1', 'password2', 'password3', 'password4', 'password5',
+    'password6', 'password7', 'password8', 'password9', 'password10',
+    'password11', 'password12', 'password13', 'password14', 'password15',
+    'password16', 'password17', 'password18', 'password19', 'password20',
+    'password21', 'password22', 'password23', 'password24', 'password25',
+    'password26', 'password27', 'password28', 'password29', 'password30'
+  ];
+  
+  // Check if the password is one of the valid passwords
+  if (validPasswords.includes(password)) {
     appProps.pageProps.hasReadPermission = true;
   }
 
